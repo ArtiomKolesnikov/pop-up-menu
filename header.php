e2200743b7d53da91c240cf36c21cc915b64333f
Green <77777green77777@gmail.com>
2016-08-10 01:44:53 +0300

    fifth commit pop-up menu

--- header.php
+++ header.php
@@ -1,10 +1,16 @@
 <header>
     <menu class="menu">
         <ul class="clearfix">
-            <li><a href="">Main</a></li>
-            <li class="has-child"><a href="">Notebooks</a></li>
-            <li><a href="">Contacts</a></li>
-
+            <li><a href="#">Main</a></li>
+            <li class="has-child"><a href="#">Notebooks</a>
+                <ul class="dropdown">
+                    <li><a href="#">MSI</a></li>
+                    <li><a href="#">ASUS</a></li>
+                    <li><a href="#">DELL</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Contacts</a></li>
+            <li><a href="#">Enter</a></li>
         </ul>
     </menu>
 
--- mainJS.js
+++ mainJS.js
@@ -1,1 +1,12 @@
+function User(name){
+    this.name = name;
+}
 
+User.prototype.work = function(who){
+    console.log(who.name + " work");
+}
+
+var Vasya = new User("Vasya");
+var Petya = new User("Petya");
+
+Vasya.work(Vasya);
