e78edfe54ac1135956ca29a72e537eccd01b8008
Green <77777green77777@gmail.com>
2016-08-09 13:30:17 +0300

    second commit pop-up menu

--- header.php
+++ header.php
@@ -1,3 +1,11 @@
 <header>
+    <menu class="menu">
+        <ul class="clearfix">
+            <li><a href="">Main</a></li>
+            <li class="has-child"><a href="">Notebooks</a></li>
+            <li><a href="">Contacts</a></li>
+            <li><a href="">Enter</a></li>
+        </ul>
+    </menu>
 
 </header>
